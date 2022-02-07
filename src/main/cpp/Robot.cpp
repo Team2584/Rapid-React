1e34a1a5b1b89a37f7e8d7a4f2a814c6daa0f8e7
Mahdi <42314845+OofRekt@users.noreply.github.com>
2022-02-07 08:32:17 -0800

    commit

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,10 +1,14 @@
 #include <Robot.h>
 #include <frc/smartdashboard/SmartDashboard.h>
 #include "rev/CANSparkMax.h"
-#include "networktables/NetworkTable.h"
-#include "networktables/NetworkTableInstance.h"
 #include <frc/Joystick.h>
 #include <frc/drive/differentialdrive.h>
+#include "frc/smartdashboard/Smartdashboard.h"
+#include "networktables/NetworkTable.h"
+#include "networktables/NetworkTableInstance.h"
+#include "networktables/NetworkTableEntry.h"
+#include "networktables/NetworkTableValue.h"
+#include "wpi/span.h"
 
 using namespace frc;
 
@@ -22,21 +26,30 @@ using namespace frc;
     //2022 Network Table //needs fix
   //  }
 
-  double tx = table->GetNumber("tx",0.0);                   //Get horizontal off set from target
-  double ty = table->GetNumber("ty",0.0);                   //Get vertical offset from target
-  double ta = table->GetNumber("ta",0.0);                   //Get area of target on screen
-  double ts = table->GetNumber("ts",0.0);                   //Get skew of target
-  double tv = table->GetNumber("tv", 0.0);
+//   double tx = table->GetNumber("tx",0.0);                   //Get horizontal off set from target
+//   double ty = table->GetNumber("ty",0.0);                   //Get vertical offset from target
+//   double ta = table->GetNumber("ta",0.0);                   //Get area of target on screen
+//   double ts = table->GetNumber("ts",0.0);                   //Get skew of target
+//   double tv = table->GetNumber("tv", 0.0);
+
+  nt::NetworkTableEntry tx;
+  nt::NetworkTableEntry ty;
+  nt::NetworkTableEntry ta;
+  nt::NetworkTableEntry ts;
+  nt::NetworkTableEntry tv;
+
+  tx = table->GetEntry("tx");
+  ty = table->GetEntry("ty");
+  ta = table->GetEntry("ta");
+  ts = table->GetEntry("ts");
+  tv = table->GetEntry("tv");
 
   frc::Joystick *m_stick;
 
   frc::DifferentialDrive drive{m_leftfront, m_rightfront};
 
-
 void Robot::RobotInit() {
-  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
-  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
-  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+  //frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
 
   m_stick = new Joystick(0);
 
@@ -47,47 +60,49 @@ void Robot::RobotInit() {
   //365 rotations for 18.85 inches
 }
 
-void Robot::RobotPeriodic() {}
+void Robot::RobotPeriodic() {
+  frc2::CommandScheduler::GetInstance().Run();
+}
+
+void Robot::DisabledInit() {}
+
+void Robot::DisabledPeriodic() {}
 
 void Robot::AutonomousInit() {
-  m_autoSelected = m_chooser.GetSelected();
-  // m_autoSelected = SmartDashboard::GetString("Auto Selector",
-  //     kAutoNameDefault);
-  fmt::print("Auto selected: {}\n", m_autoSelected);
+  m_autonomousCommand = m_container.GetAutonomousCommand();
 
-  if (m_autoSelected == kAutoNameCustom) {
-    // Custom Auto goes here
-  } else {
-    // Default Auto goes here
+  if (m_autonomousCommand != nullptr) {
+    m_autonomousCommand->Schedule();
   }
 }
 
-void Robot::AutonomousPeriodic() {
-  if (m_autoSelected == kAutoNameCustom) {
-    // Custom Auto goes here
-  } else {
-    // Default Auto goes here
-  }
-}
+void Robot::AutonomousPeriodic() {}
 
-void Robot::TeleopInit() {}
+void Robot::TeleopInit() {
+  // if (m_autonomousCommand != nullptr) {
+  //   m_autonomousCommand->Cancel();
+  //   m_autonomousCommand = nullptr;
+  // }
 
-void Robot::TeleopPeriodic() {
+  float KpX = -0.02;
+  float KpY = -0.01;
+
+  //THIS IS A PROBLEM #Fix this and the code should work as intended { 
+  //std::shared_ptr<NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
+    //2021 Network Table
+  auto inst = nt::NetworkTableInstance::GetDefault();
+  auto table = inst.GetTable("limelight");
+    //2022 Network Table //needs fix
+  //  }
+//   double tx = table->GetNumber("tx", 0.0);
+//   double ty = table->GetNumber("ty", 0.0);  
+  nt::NetworkTableEntry tx;
+  nt::NetworkTableEntry ty;
+  
+  tx = table->GetEntry("tx");
+  ty = table->GetEntry("ty");  
+
   if (m_stick->GetRawButton(2)){
-
-    float KpX = -0.02;
-    float KpY = -0.01;
-
-    //THIS IS A PROBLEM #Fix this and the code should work as intended { 
-    //std::shared_ptr<NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
-      //2021 Network Table
-    auto table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
-      //2022 Network Table //needs fix
-    //  }
-
-    double tx = table->GetNumber("tx", 0.0);
-    double ty = table->GetNumber("ty", 0.0);   
-
 
     float heading_error = tx;
     float steering_adjust = KpX * tx;
@@ -98,12 +113,12 @@ void Robot::TeleopPeriodic() {
 
     //float rotations = (steering_adjust/(6*pi))*8.68;
 
-    // while (tx > 1.5 || tx < -1.5){
+    while (tx > 7.5 || tx < -7.5){
       drive.TankDrive(-steering_adjust, steering_adjust);
-    // }
-    // while (tx < 1.5 && tx > -1.5){
-    //   drive.TankDrive(driving_adjust, driving_adjust);
-    // }
+    }
+    while (tx < 7.5 && tx > -7.5){
+      drive.TankDrive(driving_adjust, driving_adjust);
+    }
 
 
     //drive.TankDrive(-steering_adjust, steering_adjust);
@@ -122,11 +137,7 @@ void Robot::TeleopPeriodic() {
   }
 }
 
-void Robot::DisabledInit() {}
-
-void Robot::DisabledPeriodic() {}
-
-void Robot::TestInit() {}
+void Robot::TeleopPeriodic() {}
 
 void Robot::TestPeriodic() {}
 
